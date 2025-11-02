719367f9fecb1a0ce438a5ae5138f08abf2d6dd5
hermogene2001 <hermogene2001@gmail.com>
2025-11-02 17:17:50 +0200

    Index page is created

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HEALTHRWANDA - Home</title>
+</head>
+<body>
+    <h1>Welcome to HEALTHRWANDA</h1>
+    <nav>
+        <a href="registration.php">Register</a> | 
+        <a href="login.php">Login</a>
+    </nav>
+</body>
+</html>
